5d82970334c4fba433651fb0582898753d34a499
emimaldo <emimaldo@gmail.com>
2025-04-02 10:12:49 -0300

    Fix the funtion to add an adjacent node

--- src/GraphNode.ts
+++ src/GraphNode.ts
@@ -8,41 +8,39 @@ export class GraphNode<T>
     private adjacents: Map<T, GraphNode<T>>;
     private graph?: Graph<T>;
 
-    constructor (value: T) {
+    constructor(value: T) {
         this.value = value;
         this.status = GraphNodeStatus.unvisited;
         this.adjacents = new Map<T, GraphNode<T>>();
     }
 
-    getValue (): T {
+    getValue(): T {
         return this.value;
     }
 
-    getGraph (): Graph<T> | undefined {
+    getGraph(): Graph<T> | undefined {
         return this.graph;
     }
 
-    setGraph (graph: Graph<T>) {
+    setGraph(graph: Graph<T>) {
         this.graph = graph;
     }
 
-    getStatus (): GraphNodeStatus {
+    getStatus(): GraphNodeStatus {
         return this.status;
     }
 
-    setStatus (status: GraphNodeStatus) {
+    setStatus(status: GraphNodeStatus) {
         this.status = status;
     }
 
-    getAdjacents (): Map<T, GraphNode<T>> {
+    getAdjacents(): Map<T, GraphNode<T>> {
         return this.adjacents;
     }
 
-    addAdjacent (node: GraphNode<T>) {
-        if (this.adjacents.get(node.value)) {
+    addAdjacent(node: GraphNode<T>): void {
+        if (!this.adjacents.has(node.value)) {
             this.adjacents.set(node.value, node);
         }
     }
-
-    
 }
